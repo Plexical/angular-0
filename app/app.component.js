a8b08c024bb0064110e928c1a1b4a8b869a20043
Dorota Kawalec <dorota.kawalec@jking.pl>
2016-05-25 19:45:57 +0200

    Added some Semantic UI integration

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,14 @@
     app.AppComponent = 
         ng.core.Component({
             selector: 'test-app',
-            template: '<h1>The app is running!</h1>'
+            directives: [app.AccordionComponent],
+            template: 
+            	`<div class="ui container">
+	            	<h1 class="ui center aligned header">
+	            		The app is running!
+	            	</h1>
+	            	<semantic-accordion></semantic-accordion>
+            	</div>`
         })
         .Class({
             constructor: function() {}
